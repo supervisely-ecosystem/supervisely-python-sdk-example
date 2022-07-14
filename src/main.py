3b79ea22acf33afb5c6d0568fd5d6032cc76218a
Maxim Kolomeychenko <maxim@supervise.ly>
2022-07-14 17:28:37 +0300

    init

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 import os
+from dotenv import load_dotenv
 import supervisely as sly
 
 
-if __name__ == "__main__":
-    x = 123
-    print("hi there")
+print("Load server address and API token from env")
+# load .env file with secrets (recommended)
+load_dotenv(os.path.join(os.getcwd(), "debug_secret.env"))
+# or init ENVs right in your code
+# os.environ["SERVER_ADDRESS"] = "address of your Supervisely instance"
+# os.environ["API_TOKEN"] = "your personal api token"
+
+api = sly.Api.from_env()
+
+# let's test that authentication was successful and we can communicate with the platform
+my_teams = api.team.get_list()
+for team_info in my_teams:
+    print(team_info.id, team_info.name)
